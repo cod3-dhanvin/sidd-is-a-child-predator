66598137b32d5d9f8fb1959c97e6eae0e5e757f3
DhanvinPrakash <prakash_dhanvin@s2023.ssts.edu.sg>
2024-07-06 11:39:23 +0800

    Initial Commit

---
+++ sidd is a child predator/sidd_is_a_child_predatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  sidd_is_a_child_predatorApp.swift
+//  sidd is a child predator
+//
+//  Created by dhanvin_macbook on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct sidd_is_a_child_predatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sidd is a child predatorUITests/sidd_is_a_child_predatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sidd_is_a_child_predatorUITestsLaunchTests.swift
+//  sidd is a child predatorUITests
+//
+//  Created by dhanvin_macbook on 6/7/24.
+//
+
+import XCTest
+
+final class sidd_is_a_child_predatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
